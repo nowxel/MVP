3e9ad6ea20b0d76a4d38a414ad30f84482166897
nowxel <58902795+nowxel@users.noreply.github.com>
2021-02-11 09:17:40 +0200

    Create api_helper.dart
    Initial commit

---
+++ lib/data/network/api_helper.dart
@@ -0,0 +1,11 @@
+import 'dart:async';
+
+import 'package:http/http.dart' as http;
+
+class ApiHelper {
+  Future fetchtext() async {
+    var response =
+        await http.post("https://www.reweyou.in/fasalapp/fetchtext.php");
+    return response;
+  }
+}
